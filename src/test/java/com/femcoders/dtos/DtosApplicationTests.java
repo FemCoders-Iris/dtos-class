2741ab13f4ddeab1dc12f0bd1656d14a0c1ffdf8
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-07-09 23:38:49 +0200

    buil: set up springboot project

---
+++ src/test/java/com/femcoders/dtos/DtosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femcoders.dtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
